c5a7f42a4a56b312046934fc44b930046b121d04
Dimitar Popchev <dpopchev@gmail.com>
2023-08-10 23:55:07 +0300

    init

---
+++ src/i3/scripts/backlight.sh
@@ -0,0 +1,21 @@
+# change backlight using available command
+
+source ~/.config/i3/scripts/_i3_logger.sh
+
+CMD_CANDIDATES=(brightnessctl xbacklight)
+
+unset backlight_cmd
+for candidate in "${CMD_CANDIDATES[@]}"; do
+    if [ -x "$(command -v $candidate)" ]; then
+        backlight_cmd=$candidate
+        break
+    fi
+done
+
+case "$1" in
+    up) $backlight_cmd s +5%;;
+    down) $backlight_cmd s -5%;;
+    half) $backlight_cmd s 50%;;
+    full) $backlight_cmd s 100%;;
+    *) log_unhandled $backlight_cmd "did not manage subcmd" "$1";;
+esac
