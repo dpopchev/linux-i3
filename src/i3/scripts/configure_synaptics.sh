faae2f6c7e7f4f511999802daa5d95155b0cf215
Dimitar Popchev <dpopchev@gmail.com>
2023-08-14 21:23:21 +0300

    refactor

---
+++ src/i3/scripts/configure_synaptics.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+synaptics_id=$(xinput list | sed -rn '/Synaptics/s/.*id=([0-9]+).*/\1/p')
+[[ -z $synaptics_id ]] && exit 1
+xinput set-prop $synaptics_id 'Synaptics ClickPad' 0
+xinput set-prop $synaptics_id 'Synaptics Click Action' 0 0 0
+xinput set-prop $synaptics_id 'Synaptics Palm Detecion' 1
+xinput set-prop $synaptics_id 'Synaptics Coasting Speed' 0 0
