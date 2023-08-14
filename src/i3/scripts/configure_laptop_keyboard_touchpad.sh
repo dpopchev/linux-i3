4c7ed9354d1e811b5e8505288845b40351850e9f
Dimitar Popchev <dpopchev@gmail.com>
2023-08-14 21:15:17 +0300

    move laptop touchapad keyboard config into script

--- src/i3/scripts/configure_laptop_keyboard_touchpad.sh
+++ src/i3/scripts/configure_laptop_keyboard_touchpad.sh
@@ -4,22 +4,22 @@
 # concrete calls are ignored via gitignore filter found in attributes
 
 disable_touchpad_middle_button_click () {
-    # xinput set-button-map 12 1 0 3 4 5 6 7
+    xinput set-button-map $1 1 0 3 4 5 6 7
 }
 
 disable_inertial_scrolling () {
-    # xinput set-prop 11 337 0 0
 }
 
 disable_touchpad_clicking () {
-    # xinput set-prop 11 330 0 0 0
 }
 
 disable_touchpad_while_typing () {
     syndaemon -i 0.5 -K -R &
 }
 
-disable_touchpad_middle_button_click
-disable_inertial_scrolling
-disable_touchpad_clicking
+synaptics_id=$(xinput list | sed -rn '/Synaptics/s/.*id=([0-9]+).*/\1/p')
+[[ -z $synaptics_id ]] && exit 1
+disable_touchpad_middle_button_click $synaptics_id
+disable_inertial_scrolling $synaptics_id
+disable_touchpad_clicking  $synaptics_id
 disable_touchpad_while_typing
