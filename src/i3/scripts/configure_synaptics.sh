f2e3ce3df37339cf59c32eb503323b1b56f0271c
Dimitar Popchev <dpopchev@gmail.com>
2023-08-15 00:37:45 +0300

    extend synaptics options

--- src/i3/scripts/configure_synaptics.sh
+++ src/i3/scripts/configure_synaptics.sh
@@ -2,6 +2,12 @@
 
 synaptics_id=$(xinput list | sed -rn '/Synaptics/s/.*id=([0-9]+).*/\1/p')
 [[ -z $synaptics_id ]] && exit 1
+xinput set-prop $synaptics_id 'Synaptics ClickPad' 0
+xinput set-prop $synaptics_id 'Synaptics Tap Action' 0 0 0
 xinput set-prop $synaptics_id 'Synaptics Palm Detection' 1
+xinput set-prop $synaptics_id 'Synaptics Palm Dimensions' 5 5
 xinput set-prop $synaptics_id 'Synaptics Coasting Speed' 0 0
+xinput set-prop $synaptics_id 'Synaptics Gestures' 0
+xinput set-button-map $synaptics_id 1 0 3 4 5 6 7
 syndaemon -i 0.5 -K -R -d
+
