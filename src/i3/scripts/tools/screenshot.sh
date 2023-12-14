d294daf1f915f105b898e34679988b48ad086a75
Dimitar Popchev <dpopchev@gmail.com>
2023-12-14 19:07:59 +0200

    update screenshot scripts

--- src/i3/scripts/tools/screenshot.sh
+++ src/i3/scripts/tools/screenshot.sh
@@ -5,4 +5,5 @@ screenshots_home="${HOME}/Pictures/screenshots"
 mkdir --parents $screenshots_home
 
 scrot -s $screenshots_home/'%F_%T_$wx$h.png' \
-    -e 'xclip -selection clipboard -target image/png -i $f' && notify-send 'Screenshot taken'
+    -e 'xclip -selection clipboard -target image/png -i $f' \
+    && notify-send "Screenshot saved: $screenshots_home"
